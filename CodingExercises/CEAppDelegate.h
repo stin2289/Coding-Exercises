b8d8fb26a758cc7463a535789324a446cb15c757
Austin Marusco <amarusco@gmail.com>
2013-11-09 13:02:11 -0800

    Initial Commit

---
+++ CodingExercises/CEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CEAppDelegate.h
+//  CodingExercises
+//
+//  Created by Austin Marusco on 11/9/13.
+//  Copyright (c) 2013 Austin Marusco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CodingExercises/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CodingExercises
+//
+//  Created by Austin Marusco on 11/9/13.
+//  Copyright (c) 2013 Austin Marusco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CEAppDelegate class]));
+    }
+}
